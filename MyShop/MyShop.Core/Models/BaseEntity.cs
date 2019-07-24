2bbfae6533d8a2ea0b029747aa4054dfdb6c7dbe
Lwatt2934 <lwat413@gmail.com>
2019-07-24 16:08:56 -0500

    Generic complete.

--- MyShop/MyShop.Core/Models/BaseEntity.cs
+++ MyShop/MyShop.Core/Models/BaseEntity.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    class BaseEntity
+    public abstract class BaseEntity
     {
+        public string Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public BaseEntity()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
+        }
     }
 }
--- MyShop/MyShop.Core/Models/ProductCategory.cs
+++ MyShop/MyShop.Core/Models/ProductCategory.cs
@@ -4,19 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Web;
-
 namespace MyShop.Core.Models
 {
-    public class ProductCategory
+    public class ProductCategory : BaseEntity
     {
-            public string Id { get; set; }
-            public string Category { get; set; }
-
-            public ProductCategory()
-            {
-                this.Id = Guid.NewGuid().ToString();
-            }
-        
+        public string Category { get; set; }
     }
 }
